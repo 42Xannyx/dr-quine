6b59a69fe33f7fbe0ac9d401f6878a0315689eb8
xannyx <xander@xvproductions.com>
2025-08-04 14:04:25 +0200

    Added comments

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-/*Comment*/void Colleen(){}int main(){/*inmain*/
-char*s="#include <stdio.h>%c/*Comment*/void Colleen(){}int main(){/*inmain*/%cchar*s=%c%s%c;%cprintf(s,10,10,34,s,34,10);return 0;}";
-printf(s,10,10,34,s,34,10);}
+/*
+ a
+*/
+void Colleen(){}int main(){
+/*
+ b
+*/
+char*s="#include <stdio.h>%c/*%c%ca%c*/%cvoid Colleen(){}int main(){%c/*%c%cb%c*/%cchar*s=%c%s%c;%cprintf(s,10,10,32,10,10,10,10,32,10,10,34,s,34,10,10);}%c";
+printf(s,10,10,32,10,10,10,10,32,10,10,34,s,34,10,10);}
