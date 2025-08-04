8364fcf4e567b06c0f87cbf50f517625c408e020
xannyx <xander@xvproductions.com>
2025-08-04 13:35:26 +0200

    Added Grace

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 /*Comment*/void Colleen(){}int main(){/*inmain*/
 char*s="#include <stdio.h>%c/*Comment*/void Colleen(){}int main(){/*inmain*/%cchar*s=%c%s%c;%cprintf(s,10,10,34,s,34,10);return 0;}";
-printf(s,10,10,34,s,34,10);return 0;}
+printf(s,10,10,34,s,34,10);}
---
+++ C/Grace/Grace.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#define FT(x)int main(){char*s="#include <stdio.h>%c#define FT(x)int main(){char*s=%c%s%c;FILE*f=fopen(%cGrace_kid.c%c,%cw%c);fprintf(f,s,10,34,s,34,34,34,34,34,10,10);}%cFT(x)%c";FILE*f=fopen("Grace_kid.c","w");fprintf(f,s,10,34,s,34,34,34,34,34,10,10);}
+FT(x)
