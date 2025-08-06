84f9182c0986fb8b4baa43096fca37c86edece79
xannyx <xander@xvproductions.com>
2025-08-06 16:36:33 +0200

    Added Colleen.s

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,19 +1,19 @@
 .section .rodata
-
-source:
-    .ascii ".section .rodata\n\nsource:\n    .ascii \".\"\n.equ len, . - source\n\n.section .text\n.global _start\n\n_start:\nmov $1, %rax\nmov $1, %rdi\nmov $source, %rsi\nmov $len, %rdx\nsyscall\nmov $60, %rax\nxor %rdi, %rdi\nsyscall\n"
-
-.equ len, . - source
+format_string:
+.asciz ".section .rodata%1$cformat_string:%1$c.asciz %2$c%3$s%2$c%1$c%1$c.section .text%1$c.global main%1$c%1$cmain:%1$cpushq %%rbp%1$cmovq %%rsp, %%rbp%1$cmovq $format_string, %%rdi%1$cmovq $10, %%rsi%1$cmovq $34, %%rdx%1$cmovq $format_string, %%rcx%1$cmov $0, %%rax%1$ccall printf%1$cmov $0, %%rax%1$cleave%1$cret%1$c"
 
 .section .text
-.global _start
+.global main
 
-_start:
-mov $1, %rax
-mov $1, %rdi
-mov $source, %rsi
-mov $len, %rdx
-syscall
-mov $60, %rax
-xor %rdi, %rdi
-syscall
+main:
+pushq %rbp
+movq %rsp, %rbp
+movq $format_string, %rdi
+movq $10, %rsi
+movq $34, %rdx
+movq $format_string, %rcx
+mov $0, %rax
+call printf
+mov $0, %rax
+leave
+ret
